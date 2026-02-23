5a4ecb67a7d717d033d6af5e8f5e77f2124e9caa
zuqothempos <alexandretheoo@icloud.com>
2026-02-23 14:49:43 +0100

    Update anothertest.lua

--- anothertest.lua
+++ anothertest.lua
@@ -1,22 +1,19 @@
 local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/zuqothempos/testt/refs/heads/main/Test.lua"))()
 
-Library:CreateWindow("Midnight Chasers", true, function(W)
-
-    local tab1 = W:AddTab("Farm")
-    tab1:AddLabel("== Auto Farm ==")
-    tab1:AddSeparator("Paramètres")
-    tab1:AddSlider("Vitesse", "Vitesse du véhicule", 100, 1000, 300, function(value)
-        getfenv().speed = value
-    end)
-    tab1:AddToggle("Auto Farm", "Active le farm", false, function(state)
-        -- ton code
-    end)
-
-    local tab2 = W:AddTab("Visuel")
-    tab2:AddToggle("Black Screen", "Écran noir", false, function(state)
-        -- ton code
-    end)
-
+local Window = Library:CreateWindow("Midnight Chasers", true) -- true = demande une clé
+local tab1 = Window:AddTab("Farm")
+tab1:AddToggle("Auto Farm", "Active le farm", false, function(state)
+    print(state)
 end)
+-- Sans système de clé
+local Window = Library:CreateWindow("Midnight Chasers", false)
+
+local tab1 = Window:AddTab("Farm")
+tab1:AddToggle("Auto Farm", "Active le farm", false, function(state)
+    print(state)
+end)
+
+
+
 
 
